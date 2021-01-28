41d360ff3120f74348ad709bc53b0b7f5ffb25ba
Faiza Qazi <48710976+FaizaQazi21@users.noreply.github.com>
2021-01-28 20:13:22 +0000

    Create Home.js

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../App.css';
+import VideoSection from '../VideoSection/VideoSection';
+
+
+function Home() {
+    return (
+        <React.Fragment>
+            <VideoSection />
+        </React.Fragment>
+    )
+}
+
+export default Home
